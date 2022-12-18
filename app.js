9b67bff63ad9e59840b8c7770bd79153099a8681
Muhammed Lütfü ÖĞRETİCİ <mogretici@yandex.com.tr>
2022-12-18 23:40:56 +0300

    templateEngine

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function () {
-  console.log("first");
-};
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+//template engine
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+
+//routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
