76746233fc7536b06223edc5c437f2513703862f
Muhammed Lütfü ÖĞRETİCİ <mogretici@yandex.com.tr>
2023-01-06 15:33:23 +0300

    task10

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+const mongoose = require("mongoose");
 const express = require("express");
-const path = require("path");
 const ejs = require("ejs");
+const path = require("path");
+const Photo = require("./models/Photo");
+
 const app = express();
 const port = 3000;
 
-//template engine
+//dbConnect
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://localhost/pcat-test-db");
+
+//templateEngine
 app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const photos = await Photo.find();
+  res.render("index", {
+    photos,
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
@@ -20,7 +32,16 @@ app.get("/about", (req, res) => {
 app.get("/add", (req, res) => {
   res.render("add");
 });
-
+app.get("/photo/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
+  });
+});
+app.post("/add", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
 });
